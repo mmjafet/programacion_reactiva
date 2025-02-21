106ed755be96ca316c44e5c02dfadca741368760
jafet <91506409+mmjafet@users.noreply.github.com>
2025-02-21 08:08:47 -0600

    eliminando código redundante en la función de renderizado de tareas

--- javascript/index.js
+++ javascript/index.js
@@ -25,31 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Limpiar el contenido de listaDatos antes de agregar las tareas
         listaDatos.innerHTML = '';
 
-        data.map((taskItem) => {
-            const nuevoElemento = taskItem.status ?
-                `<div class="hero-card hero-card--complete">
-                    <div class="card_task">
-                        <p>${taskItem.task}</p>
-                        <span>${taskItem.description}</span>
-                    </div>
-                    <div class="card_buttons">
-                        <button class="check" onClick="actualizar(${taskItem.id})"><i class="bi bi-check-circle"></i></button>
-                        <button class="delete" onClick="eliminar(${taskItem.id})"><i class="bi bi-trash"></i></button>
-                    </div>
-                </div>` :
-                `<div class="hero-card">
-                    <div class="card_task">
-                        <p>${taskItem.task}</p>
-                        <span>${taskItem.description}</span>
-                    </div>
-                    <div class="card_buttons">
-                        <button class="check" onClick="actualizar(${taskItem.id})"><i class="bi bi-check-lg"></i></button>
-                        <button class="delete" onClick="eliminar(${taskItem.id})"><i class="bi bi-trash"></i></button>
-                    </div>
-                </div>`;
-            listaDatos.innerHTML += nuevoElemento;
-        });
-
+        
         // Limpiar los campos del formulario después de agregar la tarea
         taskInput.value = '';
         taskDescriptionInput.value = '';
@@ -66,6 +42,7 @@ const actualizar = (id) => {
     });
     data = newData;
 };
+
 
 const eliminar = (id) => {
     const newData = data.filter(task => task.id !== id);
